eb28524d6c7046ed96c3bbf00ae42aecec58479f
Y <@gmail.com>
2025-09-25 09:24:17 +0300

    Создал массив из 5000 чисел со случайным перебором от -1000 до 1000

---
+++ LR 1/Lab 2/Lab 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5000];
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(-1000, 1000);
+            }
+            foreach(int element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
