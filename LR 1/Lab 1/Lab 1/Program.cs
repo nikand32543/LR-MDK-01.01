754576a676e25d3f07b62f1583015e60d61f1d63
Y <@gmail.com>
2025-09-25 08:43:11 +0300

    Создал массив с вводом целых чисел

---
+++ LR 1/Lab 1/Lab 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // С клавиатуры ввести массив из n целых чисел. Найти произведение отрицательных элементов
+            Console.WriteLine("Введите размер массива ");
+            int razArray = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[razArray];
+            for (int i = 0; i < razArray; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            
+        }
+    }
+}
