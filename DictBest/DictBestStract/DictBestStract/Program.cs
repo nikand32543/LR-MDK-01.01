db6390eddd9c064efdb6910c6cca415e0d2ac898
Andrey-Shelgunov <selgunova3@gmail.com>
2025-10-28 10:16:49 +0300

    Создал словарь с 3-мя ключами и вывод на экран, реализовал функцию по выводу марок авто

---
+++ DictBest/DictBestStract/DictBestStract/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace DictBestStract
+{
+    class Program
+    {
+        static public void AllElementValues(Dictionary<string, List<string>> Dictionary)
+        {
+            Console.WriteLine("Какая цена вам интересна (Дорогие, Средние, Дешёвые): ");
+            string UserRequest = Console.ReadLine();
+            List<string> brands = Dictionary[UserRequest];
+            Console.WriteLine("Число марок автомобилей: " + Dictionary.Values.Count);
+            Console.Write("Все марки автомобилей: " + string.Join(", ", brands));
+        }
+        static void Main(string[] args)
+        {
+            //Создание словаря
+            Dictionary<string, List<string>> Dictionary = new Dictionary<string, List<string>>();
+            //Заполнение словаря
+            Dictionary.Add("Дорогие", new List<string>() { "Феррари" });
+            Dictionary.Add("Средние", new List<string>() { "Шевроле", "Мазда" });
+            Dictionary.Add("Дешёвые", new List<string>() { "Копейка", "Семёрка", "Двенашка" });
+            // Вывод на экран всех ключей
+            Console.Write(string.Join(", ", Dictionary.Keys));
+            // Вывод на экран значения через запятую по любому ключу
+            Console.WriteLine();
+            Console.WriteLine("Какой тип машины вам интересен: ");
+            string a = Console.ReadLine();
+            List<string> price = Dictionary[a];
+            Console.WriteLine(string.Join(",", price));
+            //Зпрос ключа пользователем по запросу выводит число элементов значения указанного ключа
+            AllElementValues(Dictionary);
+        }
+    }
+}
---
+++ DictBest/DictBestStract/DictBestStract/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DictBestStract")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DictBestStract")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ee951ec7-716f-4b13-8d30-16c5739ddd06")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
