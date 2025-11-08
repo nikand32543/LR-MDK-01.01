e654a82c115b8d24f91fc473a638dc41ec0ad95d
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-08 12:48:42 +0300

    Определил функцию

--- StructProject/StructProject/Vector.cs
+++ StructProject/StructProject/Vector.cs
@@ -1,8 +1,15 @@
-﻿namespace StructProject
+﻿using System.Collections.Generic;
+
+namespace StructProject
 {
     public struct Vector
     {
         public Point begin;
         public Point end;
+
+        static public List<double> VectorLenght(Vector[] allVectors)
+        {
+
+        }
     }
 }
