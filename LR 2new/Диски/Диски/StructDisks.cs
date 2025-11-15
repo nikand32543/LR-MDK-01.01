333b73973a49ad5223c0c1d06986fc39805aa04f
a.boikov <alex8oikov@yandex.ru>
2025-11-15 12:02:22 +0300

    Создал структуру

---
+++ LR 2new/Диски/Диски/StructDisks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Диски
+{
+    struct Disks
+    {
+        public string Name;
+        public string Year;
+        public int Requests; /// число обращений
+    }
+}
