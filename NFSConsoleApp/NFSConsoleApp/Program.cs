45c1f090aaddfe890d91d2f73e7c65988c6a8510
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-22 11:47:21 +0300

    Реализовал функцию для гонок

--- NFSConsoleApp/NFSConsoleApp/Program.cs
+++ NFSConsoleApp/NFSConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace NFSConsoleApp
 {
@@ -16,16 +17,38 @@ namespace NFSConsoleApp
             Car auto = new Car(); /// объект Лада
             auto.SetModel("Лада");
             auto.SetSpeed(150);
-            Print(auto);
+           // Print(auto);
 
             Car merc = new Car(); /// объект Merc
             merc.SetModel("Merc");
             merc.SetSpeed(300);
-            Print(merc);
+           // Print(merc);
 
             List<Car> cars = new List<Car>(); /// Список для хранения объектов
             cars.Add(auto);
             cars.Add(merc);
+
+            LadaVsMerc(cars);
+        }
+        static public void LadaVsMerc(List<Car> cars) /// функция для гонок
+        {
+            int speed = 1;
+            while (true)
+            {
+                foreach (Car s in cars)
+                { 
+                    for (int i = 0; i < s.GetSpeed() / 50 * speed; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(s.GetModel());
+                    Console.WriteLine();
+                    Console.WriteLine("-------------------------------------------------------")
+                    speed++;
+                }
+                Thread.Sleep(60);
+                Console.Clear();
+            }
         }
     }
 }
