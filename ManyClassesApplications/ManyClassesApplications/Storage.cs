35590840a2dc74bc4c922b36772dd3904662352a
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-25 12:55:55 +0300

    Создал класс Storage и заполнил его методами

--- ManyClassesApplications/ManyClassesApplications/Storage.cs
+++ ManyClassesApplications/ManyClassesApplications/Storage.cs
@@ -1,9 +1,26 @@
 ﻿
 
+using System.Collections.Generic;
+
 namespace ManyClassesApplications
 {
     class Storage /// класс (место хранения товара)
     {
+        private int identifier_; /// идентификатор
+        private string location_; /// локация
+        private Dictionary<Product, int> products_; /// Словарь с ключом Product
 
+        public void SetIdentifier(int identifier) /// Метод добавления идентификатора
+        {
+            identifier_ = identifier;
+        }
+        public void SetLocation(string location) /// Метод добавления локации
+        {
+            location_ = location;
+        }
+        public void SetProductQuantity(Product product, int quantity)
+        {
+            products_.Add(product, quantity);
+        }
     }
 }
