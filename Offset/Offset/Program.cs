452bceec50a6f3b113da343b7fc09fa786595b9f
Y <@gmail.com>
2026-04-07 11:23:22 +0300

    НЕ сдал зачёт подучить

---
+++ Offset/Offset/Program.cs
@@ -0,0 +1,46 @@
+﻿using OffsetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Offset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Zaporogec = new Car("Запорожец", 150000, "Лада");
+
+            Console.WriteLine(Zaporogec.GetName());
+
+            List<Car> allCars = new List<Car>
+            {
+                new Car ("Vesta", 1500000, "Лада"),
+                new Car ("Niva", 250000, "Лада"),
+                new Car ("Lamborgini", 15000000, "Lamborgini")
+            };
+            allCars.Add(Zaporogec);
+            Console.WriteLine("Название автомобиля: " + allCars[1].GetName());
+            List<Car> cars = GetAllCarsMoreThanX(500000, allCars);
+
+            Console.WriteLine(cars[0].GetName());
+
+
+        }
+        static public List<Car> GetAllCarsMoreThanX(double x, List<Car> allCars)
+        {
+            List<Car> result = new List<Car>();
+            foreach (Car t in allCars)
+            {
+                if (t.GetPrice() > x)
+                {
+                    result.Add(t);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Offset/OffsetLib/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffsetLib
+{
+    public class Car
+    {
+        private string name_;
+        private int price_;
+        private string manufacturer_;
+
+        public Car (string name, int price, string manufacturer)
+        {
+            name_ = name;
+            price_ = price;
+            manufacturer_ = manufacturer;
+        }
+        public string GetName()
+        {
+            return name_;
+        }
+        public int GetPrice()
+        {
+            return price_;
+        }
+    }
+}
